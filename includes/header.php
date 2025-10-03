6fdbf4a86ddff4042ae3fb1e33516de45f40e3ae
thiyagasivam <thiyagasivam@gmail.com>
2025-10-04 00:19:59 +0530

    feat: add PWA support with manifest, humans.txt, and security.txt; update sitemap and robots.txt for new files

--- includes/header.php
+++ includes/header.php
@@ -400,6 +400,13 @@
     </script>
     <?php endif; ?>
     
+    <!-- PWA Manifest -->
+    <link rel="manifest" href="<?php echo SITE_URL; ?>/manifest.json">
+    
+    <!-- Additional Meta Links -->
+    <link rel="author" href="<?php echo SITE_URL; ?>/humans.txt">
+    <link rel="security" href="<?php echo SITE_URL; ?>/security.txt">
+    
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
